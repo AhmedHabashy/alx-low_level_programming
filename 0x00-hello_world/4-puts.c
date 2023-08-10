39da463d2a8836cfdaec253bc65af8284d9da5d1
Ahmed_Habashy <ahabashy6197@gmail.com>
2023-08-10 14:09:06 -0700

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
+/* main - Entrey point
+ * Description: prints a message
+ * Return: always 0
+ */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
