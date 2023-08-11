500b96d6019516c5908b1765f6a07ab0969cd18c
Ahmed_Habashy <ahabashy6197@gmail.com>
2023-08-11 12:20:15 -0700

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Entrey point
+*Description: hexa
+*Return: always 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	for (i = 97; i < 98; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
