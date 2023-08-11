f27d2ac7ad70e19926dfb22bba7cca2e6d80a081
Ahmed_Habashy <ahabashy6197@gmail.com>
2023-08-11 12:14:38 -0700

    update

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
 *main - Entrey point
-*Description: show me your smile 
+*Description: show me your smile
 *Return: always 0
 */
 int main(void)
 {
 	int i;
 
-	for (i = 123; i > 96 ; i--)
+	for (i = 122; i > 96 ; i--)
 		putchar(i);
 	putchar('\n');
-	
+
 	return (0);
 }
