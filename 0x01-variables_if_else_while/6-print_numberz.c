3097692a3afb8ca9e2ff21ca1af60b2c91705579
Ahmed_Habashy <ahabashy6197@gmail.com>
2023-08-11 12:06:58 -0700

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Entrey point
+*Description: ascii numbers
+*Return: always 0
+*/
+int maint(void)
+{
+	int i;
+
+	for ( i = 48; i < 58; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
