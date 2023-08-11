ad902d2ad3fb5d499b3849bd366f4f62d31d7d3f
Ahmed_Habashy <ahabashy6197@gmail.com>
2023-08-11 11:30:45 -0700

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Entrey point
+*Description: alphabet song
+*Return: always 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123 ; i++)
+		putchar(i);
+	return (0);
+}
