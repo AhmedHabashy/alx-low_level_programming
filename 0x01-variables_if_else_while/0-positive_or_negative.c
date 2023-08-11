0f044df14297a9feb71e7f5bbc3e5e143ccc171c
Ahmed_Habashy <ahabashy6197@gmail.com>
2023-08-11 09:54:02 -0700

    update

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entrey point
+ * Description: positive is better than negative
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive", n);
+	else if (n == 0)
+		printf("%d is Zero", n);
+	else
+		printf("%d is negative", n);
+	return (0);
+}
