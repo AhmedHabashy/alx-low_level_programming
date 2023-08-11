d43c1e9248c1c1e22ab8622da1eb9899267cb2f3
Ahmed_Habashy <ahabashy6197@gmail.com>
2023-08-11 12:12:29 -0700

    update

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Entrey point
+*Description: show me your smile 
+*Return: always 0
+*/
+int main(void)
+{
+	for (i = 123; i > 96 ; i--)
+		putchar(i);
+	putchar('\n');
+	
+	return (0);
+}
