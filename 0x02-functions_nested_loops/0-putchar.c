27c381ac93339048690e97d144961378b58ce2d9
Ahmed_Habashy <ahabashy6197@gmail.com>
2023-08-15 09:28:10 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Entrey point
+*Description: print putchar
+*Return: always 0
+*/
+int main(void)
+{
+	char str[] = "_putchar\n";
+
+	printf("%s", str);
+
+	return (0);
+}
