599a49bac4c321b59fff585a2f4c9b82de88d6b0
Ahmed_Habashy <ahabashy6197@gmail.com>
2023-08-15 14:08:33 -0700

    update

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * times_table - print table
+ */
+void times_table(void)
+{
+	int num, tim, res;
+
+	for (num = 0 ; num <= 9 ; num++)
+	{
+		_putchar('0');
+		for (tim = 0 ; tim <= 9; tim++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			res = num * tim;
+
+			if (res <= 9)
+				_putchar(' ');
+			else
+				_putchar((res / 10) + '0');
+			_putchar((res % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
