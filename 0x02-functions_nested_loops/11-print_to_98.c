f48ee29d8b6ba4ff4a6815da121056850f596530
Ahmed_Habashy <ahabashy6197@gmail.com>
2023-08-15 14:24:34 -0700

    update

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_to_98 - print to 98
+ * @n: count from n to 98
+ * Return: always 0
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+		for (i = n; i > 98 ; i--)
+			printf("%d, ", i);
+	else if (n < 98)
+		for (i = n ; i < 98 ; i++)
+			printf("%d, ", i);
+	printf("%d\n", 98);
+}
