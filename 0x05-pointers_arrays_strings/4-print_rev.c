ae5855d8c6ab8bdd391834093f02542ffbfec322
Ahmed_Habashy <ahabashy6197@gmail.com>
2023-08-21 12:31:06 -0700

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,12 @@ void print_rev(char *s)
 	int i = 0;
 	char line = '\n';
 
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 		i++;
 	i--;
 	while (i >= 0)
 	{
-		write(1, &str[i], 1);
+		write(1, &s[i], 1);
 		i--;
 	}
 	write(1, &line, 1);
