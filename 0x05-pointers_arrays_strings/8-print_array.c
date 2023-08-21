f9972f62647d9bed67d068af92ce142811ed565d
Ahmed_Habashy <ahabashy6197@gmail.com>
2023-08-21 14:52:15 -0700

    update

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_array - prints array elements
+ * @a: array
+ * @n: number of elements
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (a[i] != '\0')
+		i++;
+	for (j = n; j < i - 1; j++)
+	{
+		printf("%d", arr[j]);
+		if (j != i - 2)
+		{
+		printf(",");
+		printf(" ");
+		}
+	}
+}
