d449958d58bce8b660df17e1ba707b746735d86f
Ahmed_Habashy <ahabashy6197@gmail.com>
2023-08-21 13:58:22 -0700

    update

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - print half the string
+ * @str: string to be printed
+ */
+void puts2(char *str)
+{
+	int i, j;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	for (j = 0; j < i; j += 2)
+		_putchar(str[j]);
+	_putchar('\n');
+}
