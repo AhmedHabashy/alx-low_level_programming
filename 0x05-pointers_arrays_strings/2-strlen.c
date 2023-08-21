e5e1ea32b5ab0962f83ff680e07c737077d04fd5
Ahmed_Habashy <ahabashy6197@gmail.com>
2023-08-21 11:35:19 -0700

    update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - counts number of letters in a string
+ * @s: string first letter's address
+ * Return: number of letters
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (s != '\0')
+	{
+		count++;
+		s++;
+	}
+
+	return (count);
+}
