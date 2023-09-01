ecec3183841927a251f43e484903f2b9310f3b2a
AhmedHabashy <ahabashy6197@gmail.com>
2023-09-01 16:19:52 -0700

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * main - entery point
+ * @argc: argv count
+ * @argv: inputed strings
+ * Return: 0
+ */
+void main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
